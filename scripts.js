69c66d40f7959c83dee72374e2018647cc93e473
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-07-12 16:36:02 -0700

    added a remove all task button

--- scripts.js
+++ scripts.js
@@ -6,16 +6,25 @@
 
 let addNewTask = function() {
   let li = document.createElement("li")
-  let newtask = document.getElementById("addTask").value
-  let textNode = document.createTextNode(newtask)
+  let newTask = document.getElementById("addTask").value
+  let textNode = document.createTextNode(newTask)
   li.appendChild(textNode);
-  document.getElementById("list").appendChild(li);
+  document.getElementById("list").appendChild(li)
 }
-  // li.appendChild(textNode)
-  // if (newtask === '') {
-  // alert("You must write something!");
-// } else {
-//   document.getElementById("list").appendChild(li)
+
+let removeAllTask = function() {
+  let listItem = document.getElementById("list")
+if(listItem.innerHTML.length == 0){
+  alert("No list Items to remove")
+}else{
+ confirm("Are you sure you want to Remove All Tasks")
+ listItem.innerHTML = ""
+}
+}
+
+// let removeTask = function() {
+//   let listItem = document.getElementById("list")
+//   listItem.click('li', function(removeItem){
+//     removeItem.target.remove()
+//   })
 // }
-// }
-// alert("there's snakes on this mf plane!!!!")
